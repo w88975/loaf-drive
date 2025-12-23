f20f0e018d6c1d18416c17511e8d781e326b53d3
KamiSama <1457434343@qq.com>
2025-12-23 14:38:43 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/loaf-drive/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
